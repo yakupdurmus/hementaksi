c44ede1e3a9a7b3e6b6a04f9c3def0905e5bc7ff
inveon-yakupdurmus <yakup.durmus@inveon.com>
2020-12-15 23:41:29 +0300

    Harita kurulumu - Google maps oke

--- ios/hementaksi/AppDelegate.m
+++ ios/hementaksi/AppDelegate.m
@@ -12,6 +12,8 @@
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
   SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
@@ -27,6 +29,8 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyAUpN7hBCrk5RYab3v0vMAIWPn2fk4gRZA"];
+  
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
--- src/containers/FirstLaunch.js
+++ src/containers/FirstLaunch.js
@@ -1,36 +1,69 @@
-import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native'
-import { BasicButton, BasicInput, BasicText } from '../components';
-import { StackActions } from '@react-navigation/native';
-
-const FirstLaunch = (props) => {
-
-    const replaceScreen = (value) => {
-
-        props.navigation.dispatch(
-            StackActions.replace('Home', {
-                wordCount: value,
-            })
-        );
-    }
-
-    return (
-        <>
-            <View style={styles.containerStyle}>
-
-            </View>
-        </>
-    )
+import React, {useRef, useState} from 'react';
+import MapView, {PROVIDER_GOOGLE, Region} from 'react-native-maps';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+const FirstLaunch = () => {
+  const map = useRef(null);
+  const [region, setRegion] = useState({
+    latitude: 41.0391683,
+    longitude: 28.9982707,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  });
+  const onRegionChange = (changedRegion) => {
+    setRegion(changedRegion);
+  };
+  const zoomDelta = 0.005;
+  const onZoom = (zoomSign) => {
+    const zoomedRegion = {
+      latitude: region.latitude,
+      longitude: region.longitude,
+      latitudeDelta: region.latitudeDelta - zoomDelta * zoomSign,
+      longitudeDelta: region.longitudeDelta - zoomDelta * zoomSign,
+    };
+    setRegion(zoomedRegion);
+    map.current?.animateToRegion(zoomedRegion);
+  };
+  const onZoomIn = () => onZoom(1);
+  const onZoomOut = () => onZoom(-1);
+  return (
+    <>
+      <MapView
+        ref={map}
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={region}
+        onRegionChange={onRegionChange}
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={onZoomIn}>
+          <Text style={styles.text}>+</Text>
+        </TouchableOpacity>
+        <View style={styles.spacer} />
+        <TouchableOpacity style={styles.button} onPress={onZoomOut}>
+          <Text style={styles.text}>-</Text>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
 };
-
-
-
-
 const styles = StyleSheet.create({
-    containerStyle: {
-        flex: 1,
-        backgroundColor: '#f7f7f7'
-    },
-})
-
-export default React.memo(FirstLaunch);
+  map: {
+    flex: 1,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 30,
+    end: 20,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    padding: 12,
+  },
+  button: {},
+  text: {
+    textAlign: 'center',
+  },
+  spacer: {
+    marginVertical: 7,
+  },
+});
+export default FirstLaunch;
